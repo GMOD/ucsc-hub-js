a43121544bb2cf38f58395f99239202db16f87c8
Garrett Stevens <stevens.garrett.j@gmail.com>
2018-12-18 14:02:54 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+// @flow
+
+/**
+ * This function says hello.
+ * @param name Some name to say hello for.
+ * @returns The hello.
+ */
+function sayHello(name = 'Haz') {
+  return `Hello, ${name}!`
+}
+
+export default sayHello
