79e14255877859549aa21c3e934636013a7b56ed
Garrett Stevens <stevens.garrett.j@gmail.com>
2018-12-19 14:45:39 -0500

    Update babel, eslint, jest, and gitignore settings

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  collectCoverageFrom: ['src/**/*.{js,jsx}'],
+  testMatch: [
+    '<rootDir>/src/**/__tests__/**/*.{js,jsx}',
+    '<rootDir>/src/**/?(*.)(spec|test).{js,jsx}',
+  ],
+}
