a157aa1bcecb1ba04bb246396e4f96b18f3db427
Garrett Stevens <stevens.garrett.j@gmail.com>
2018-12-27 14:24:09 -0500

    Flesh out trackDb class

--- src/trackDbFile.js
+++ src/trackDbFile.js
@@ -9,15 +9,55 @@ class TrackDbFile extends RaFile {
           this.nameKey
         }" instead of "track" as the first line in each track`,
       )
+    this.forEach((track, trackName) => {
+      const stanzaKeys = Array.from(track.keys())
+      const missingKeys = []
+      const requiredKeys = ['track', 'shortLabel', 'longLabel']
+      requiredKeys.forEach(key => {
+        if (!stanzaKeys.includes(key)) missingKeys.push(key)
+      })
+      if (missingKeys.length > 0)
+        throw new Error(
+          `Track ${trackName} is missing required key(s): ${missingKeys.join(
+            ', ',
+          )}`,
+        )
+      const parentTrackKeys = ['superTrack', 'compositeTrack', 'container']
+      if (!stanzaKeys.some(key => parentTrackKeys.includes(key))) {
+        if (!stanzaKeys.includes('bigDataUrl'))
+          throw new Error(
+            `Track ${trackName} is missing required key "bigDataUrl"`,
+          )
+        if (!stanzaKeys.includes('type')) {
+          const settings = this.settings(trackName)
+          const settingsKeys = Array.from(settings.keys())
+          if (!settingsKeys.includes('type'))
+            throw new Error(
+              `Neither track ${trackName} nor any of its parent tracks have the required key "type"`,
+            )
+        }
+      }
+      let indent = ''
+      let currentTrackName = trackName
+      do {
+        currentTrackName = this.get(currentTrackName).get('parent')
+        if (currentTrackName) indent += '    '
+      } while (currentTrackName)
+      const currentTrack = this.get(trackName)
+      currentTrack.indent = indent
+      this.set(trackName, currentTrack)
+    })
   }
 
-  settings(track) {
-    const parentTracks = [track]
-    let currTrack = track
+  settings(trackName) {
+    if (!this.has(trackName))
+      throw new Error(`Track ${trackName}does not exist`)
+    const parentTracks = [trackName]
+    let currentTrackName = trackName
     do {
-      currTrack = this.get(currTrack).get('parent')
-      if (currTrack) parentTracks.push(currTrack)
-    } while (currTrack)
+      currentTrackName = this.get(currentTrackName).get('parent')
+      if (currentTrackName) parentTracks.push(currentTrackName)
+    } while (currentTrackName)
     const settings = new Map()
     parentTracks.reverse()
     parentTracks.forEach(parentTrack => {
--- src/trackDbFile.test.js
+++ src/trackDbFile.test.js
@@ -2,6 +2,13 @@ const fs = require('fs')
 const TrackDbFile = require('./trackDbFile')
 
 describe('TrackDbFile', () => {
+  it('reads a basic trackDb.txt file', () => {
+    const input = fs.readFileSync('test/basic.trackDb.txt', 'utf8')
+    const trackDb = new TrackDbFile(input)
+    expect(trackDb).toMatchSnapshot()
+    expect(trackDb.toString()).toEqual(input)
+  })
+
   it('gets settings', () => {
     const input = fs.readFileSync('test/superTrack.trackDb.txt', 'utf8')
     const trackDb = new TrackDbFile(input)
